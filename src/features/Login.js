0067a9c41e78aadc6a11601459d4058e26f62074
densbv <63791038+densbv@users.noreply.github.com>
2021-01-13 18:43:03 +0700

    Update Login.js

--- src/features/Login.js
+++ src/features/Login.js
@@ -1,9 +1,52 @@
-import React from "react";
-import { LoginForm } from "./LoginForm";
+import React, { useState } from "react";
 
-export const Login = () => {
+export const Login = ({ setToken }) => {
 
-    return (
-        <LoginForm />
-    )
-} 
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();  
+
+  const userNameChanged = e => setUserName(e.target.value)
+  const passwordChanged = e => setPassword(e.target.value)
+
+  const genToken = e => {
+    e.preventDefault();
+    const token = () => {
+      let rand = getRandomIntInclusive(1, 9999999)  
+      return {token: username + rand}
+    };
+    setToken(token);
+  }
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="card col-lg-5 mt-3">
+        <div className="card-body">
+          <form>
+            <div className="mb-3">
+              <label className="form-label">Name</label>
+              <input type="text" onChange={userNameChanged} className="form-control" id="userName" />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                onChange={passwordChanged}
+                className="form-control"
+                id="userPassword"
+              />
+            </div>
+            <button type="button" onClick={genToken} className="btn btn-primary">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
