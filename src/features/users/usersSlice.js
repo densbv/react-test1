b85f84a47f8a14243acaf8029d802badafb54ec0
densbv <63791038+densbv@users.noreply.github.com>
2021-01-14 15:07:43 +0700

    Update usersSlice.js

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -3,9 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [];
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {}
+  name: "users",
+  initialState,
+  reducers: {
+    userAdded(state, action) {
+      state.push(action.payload);
+    },
+  },
 });
 
-export default usersSlice;
+export const { userAdded } = usersSlice.actions
+
+export default usersSlice.reducer;
