366f7b5a2b817779f1fab565d08dce0a6a5c0759
densbv <63791038+densbv@users.noreply.github.com>
2021-01-14 15:07:40 +0700

    Update UserList.js

--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { AddUser } from "./AddUser";
 
 export const UserList = () => {
+  const users = useSelector((state) => state.users);
+
+  const renderUsers = users.map((user) => (
+    <div className="card" key={user.id}>
+      <div className="card-body">
+        <h5>{user.userName}</h5>
+        <p className="post-content">{user.about}</p>
+      </div>
+    </div>
+  ));
+
   return (
     <div>
-      <Link to="/users/add" className="btn btn-danger">
-        + Add
-      </Link>
+      <AddUser />
+
       <h5>UserList</h5>
+      {renderUsers}
     </div>
   );
 };
